a51d2385a2a940e8ae8e2c33046d8eb2c260add4
Ana Lozano Moreno <analozanomor@gmail.com>
2022-11-01 19:11:05 +0100

    Add bonus

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict';
 let countLight = 0;
 let countDark = 0;
+let gamesCount = 0;
 
 const selectEl = document.querySelector('.js-select');
 const btnEl = document.querySelector('.js-btn');
+const btnResetEl = document.querySelector('.js-btnReset');
 const textEl = document.querySelector('.js-text');
 const textCountLightEl = document.querySelector('.js-countLight');
 const textCountDarkEl = document.querySelector('.js-countDark');
@@ -37,9 +39,6 @@ function getRandomNumberFromArray(arr) {
 }
 
 function battleResultPaint(valueSelectedLight, valueDark) {
-  console.log({ valueSelectedLight });
-  console.log(valueDark.force);
-
   if (valueSelectedLight > valueDark.force) {
     textEl.innerHTML = '¡Ha ganado el Ejército del Bien!Enhorabuena.';
     countLight += 1;
@@ -57,6 +56,33 @@ function handleClick(event) {
   const randomNumberDark = getRandomNumberFromArray(arrayDark);
   const selectValue = parseInt(selectEl.value);
   battleResultPaint(selectValue, randomNumberDark);
+  gamesCount++;
+  if (gamesCount === 10) {
+    btnEl.classList.add('hidden');
+    btnResetEl.classList.remove('hidden');
+    gameOver();
+  }
+}
+
+function handleResetGame(event) {
+  event.preventDefault();
+  btnEl.classList.remove('hidden');
+  btnResetEl.classList.add('hidden');
+  countLight = 0;
+  countDark = 0;
+  gamesCount = 0;
+  textCountLightEl.innerHTML = countLight;
+  textCountDarkEl.innerHTML = countDark;
+}
+function gameOver() {
+  if (countLight > countDark) {
+    textEl.innerHTML = `FIN: ¡El ejécito del Bien ha ganado la guerra!`;
+  } else if (countDark > countLight) {
+    `FIN: ¡El ejécito del Mal ha ganado la guerra`;
+  } else {
+    textEl.innerHTML = `Empate`;
+  }
 }
 
 btnEl.addEventListener('click', handleClick);
+btnResetEl.addEventListener('click', handleResetGame);
